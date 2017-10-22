2e606cc37e7e59cdb44d1af5c40bcdd78b64d8d0
Kirk <rajkamal89@live.com>
2017-10-22 09:41:09 +0400

    Initial

---
+++ js/main.js
@@ -0,0 +1,7 @@
+$(function(){
+
+  
+$('#brag')
+.typetype("Kamal is a passionate and innovative Senior Software Engineer with over 6 years of experience in designing, developing and delivery of cost-effective high-performance solutions and has the ability to close high-value projects to meet the strict deadlines. He is highly optimistic in the latest technologies and is obsessive with aesthetics and user experience at the pixel level. Experienced in all aspects of project lifecycle development, highly knowledgeable in e-Commerce domain, agile methodologies, in training and mentoring personnel. Apart from his passion, He also uses his highly technical skillsets to build drones, raspberry pi projects, and electronic prototype applications in his spare time.", {t:60,e:0.08});
+  
+});
